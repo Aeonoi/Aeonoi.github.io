dd941114bd62d369b68d1b7b54ab8061c0865823
Aeonoi <dzhou527@gmail.com>
2024-10-31 16:59:54 -0400

    fix(transition): fix projects not showing up on IPhone SE

--- components/transition.tsx
+++ components/transition.tsx
@@ -13,7 +13,7 @@ const Transition: React.FC<Props> = ({ children }) => {
       whileInView={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5, ease: "easeInOut", delay: 0.2 }}
-      viewport={{ once: false, amount: 0.3 }}
+      viewport={{ once: false, amount: 0.2 }}
       className="pt-14"
     >
       {children}
