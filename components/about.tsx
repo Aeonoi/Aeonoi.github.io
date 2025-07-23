4ac415234243b81427e7bc4eeb2cc4687523ee61
Aeonoi <dzhou527@gmail.com>
2025-07-23 11:41:23 -0400

    refactor: revamp about page to be more short and concise and use icons for skills

--- components/about.tsx
+++ components/about.tsx
@@ -1,84 +1,105 @@
 "use client";
-import Background from "./background";
+
+import {
+  SiTailwindcss,
+  SiTypescript,
+  SiNextdotjs,
+  SiReact,
+  SiC,
+  SiCplusplus,
+  SiPostgresql,
+  SiMongodb,
+  SiGit,
+  SiGithub,
+  SiAmazonec2,
+  SiDrizzle,
+} from "react-icons/si";
+import { FaJava } from "react-icons/fa";
+import { Badge } from "./ui/badge";
 
 const About: React.FC = () => {
   return (
     <main id="about" className="scroll-m-14">
-      <Background styles="h-full md:h-screen flex items-center">
-        {/* <div className="grid grid-cols-2 text-black max-w-4xl mx-auto p-6"> */}
-        <div className="grid grid-cols-1 md:grid-cols-2 text-catppuccin_text p-10 w-full justify-items-center">
-          <section className="mb-8 max-w-xl">
-            <h1 className="text-4xl font-bold mb-4">Hi, I'm Dylan</h1>
-            <p className="text-lg text-catppuccin_teal">
-              I'm a junior Computer Science major studying at{" "}
-              <span className="text-catppuccin_red">
-                Rensselaer Polytechnic Institute
-              </span>{" "}
-              with a passion for
-              <span className="text-catppuccin_red">
-                {" "}
-                full-stack web development.
-              </span>{" "}
-              Driven by curiosity, I thrive on exploring new technologies and
-              pushing myself to continuously learn and grow. Whether I'm
-              building dynamic web applications or diving into a new coding
-              challenge, I'm always seeking opportunities to expand my skill set
-              and improve. My goal is to create impactful and intuitive
-              solutions while staying at the forefront of innovation in the tech
-              world.
+      <div className="mb-10">
+        <header className="text-5xl font-bold mb-5">About Me</header>
+        <div className="grid grid-cols-2 gap-20">
+          {/* About Me  */}
+          <div className="grid grid-rows-2 text-2xl gap-5">
+            <p>
+              I'm a Senior studying Computer Science at Rensselaer Polytechnic
+              Institute with a passion for frontend development. I enjoy solving
+              complex problems and turning them into simple and intuitive
+              designs.
             </p>
-          </section>
-
-          <section className="mb-8 max-w-xl">
-            <h2 className="text-3xl font-semibold mb-4">
-              Skills & Technologies
-            </h2>
-            <ul className="list-disc ml-6 text-lg text-catppuccin_teal">
-              <li>
-                <strong className="text-catppuccin_red">Languages:</strong> C++,
-                Java, JavaScript, TypeScript, C
-              </li>
-              <li>
-                <strong className="text-catppuccin_red">Frontend: </strong>
-                React.js, Next.js, Preact.js, Electron.js, Tailwind CSS, JavaFX
-              </li>
-              <li>
-                <strong className="text-catppuccin_red">Backend: </strong>
-                Node.js, Express.js, MongoDB, Mongoose ORM, PostgreSQL
-              </li>
-              <li>
-                <strong className="text-catppuccin_red">Testing:</strong> JUnit
-                5, GDB
-              </li>
-              <li>
-                <strong className="text-catppuccin_red">Tools:</strong> Git,
-                GitHub
-              </li>
-              <li>
-                <strong className="text-catppuccin_red">Cloud:</strong> Docker,
-                Amazon EC2
-              </li>
-            </ul>
-          </section>
-
-          <section className="mb-8 max-w-xl">
-            <h2 className="text-3xl font-semibold mb-4">
-              Current Focus & Interests
-            </h2>
-            <p className="text-lg text-catppuccin_teal">
-              I'm honing my skills in full-stack web development, exploring the
-              features of <span className="text-catppuccin_red">Next.js</span>{" "}
-              and <span className="text-catppuccin_red">React.js</span>, and
-              working on projects that blend intuitive UI with solid backend
-              functionality. I'm also excited about diving deeper into areas
-              like database optimization and cloud deployment. Recently, I've
-              been focusing on implementing user authentication and OAuth to
-              enhance security in my applications. In my free time, I’m
-              exploring the Angular web framework and its diverse applications.
+            <p>
+              My journey in tech started with a curiosity about how websites
+              work, which led me to explore HTML, CSS, and JavaScript. Since
+              then, I've expanded my skills to include modern frameworks like
+              React.js, Next.js, and TailwindCSS.
             </p>
-          </section>
+          </div>
+          {/* Skills */}
+          <div>
+            <header className="font-semibold text-4xl">Skills</header>
+            <div className="grid grid-cols-3 gap-5 mt-3 h-full">
+              <div>
+                <header className="text-2xl">Frontend</header>
+                <div className="pt-3 flex flex-wrap w-full gap-2">
+                  <Badge className="h-10 text-2xl">
+                    <SiTailwindcss />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiTypescript />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiNextdotjs />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiReact />
+                  </Badge>
+                </div>
+              </div>
+              <div className="h-full">
+                <header className="text-2xl">Backend</header>
+                <div className="pt-3 flex flex-wrap w-full gap-2">
+                  <Badge className="h-10 text-2xl">
+                    <FaJava />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiC />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiCplusplus />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiDrizzle />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiPostgresql />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiMongodb />
+                  </Badge>
+                </div>
+              </div>
+              <div>
+                <header className="text-2xl">Tools</header>
+                <div className="pt-3 flex flex-wrap w-full gap-2">
+                  <Badge className="h-10 text-2xl">
+                    <SiGit />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiGithub />
+                  </Badge>
+                  <Badge className="h-10 text-2xl">
+                    <SiAmazonec2 />
+                  </Badge>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </Background>
+      </div>
     </main>
   );
 };
--- components/background.tsx
+++ components/background.tsx
@@ -8,10 +8,7 @@ interface Props {
  */
 const Background: React.FC<Props> = ({ children, styles }) => {
   return (
-    <section
-      // className={`relative bg-gradient-to-r from-catppuccin_blue via-catppuccin_lavender to-catppuccin_mauve bg-black opacity-95 ${styles}`}
-      className={`relative bg-background  ${styles}`}
-    >
+    <section className={`relative bg-background  ${styles}`}>
       {children}
     </section>
   );
